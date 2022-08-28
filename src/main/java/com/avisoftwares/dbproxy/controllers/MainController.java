f26298a15b7a67b3c79c9641bb50e906eadb1d72
Avinash Kumar <avinash@spendgo.com>
2022-08-28 22:04:46 +0530

    Updated MainController.

--- src/main/java/com/avisoftwares/dbproxy/controllers/MainController.java
+++ src/main/java/com/avisoftwares/dbproxy/controllers/MainController.java
@@ -2,9 +2,7 @@ package com.avisoftwares.dbproxy.controllers;
 
 import com.avisoftwares.dbproxy.actions.ActionBuilder;
 import com.avisoftwares.dbproxy.actions.StoreAction;
-import com.avisoftwares.dbproxy.results.DatabaseStoreResult;
 import com.avisoftwares.dbproxy.results.StoreResult;
-import com.avisoftwares.dbproxy.stores.AbstractStore;
 import com.avisoftwares.dbproxy.stores.AuroraStore;
 import com.avisoftwares.dbproxy.stores.RedshiftStore;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,22 +28,14 @@ public class MainController {
         StoreResult result;
         switch (action.getStore()) {
             case "aurora":
-                result = queryAuroraStore(action);
+                result = auroraStore.executeAction(action);
                 break;
             case "redshift":
-                result = queryRedshiftStore(action);
+                result = redshiftStore.executeAction(action);
                 break;
             default:
                 result = () -> "{\"message\":\"unknown store value (allowed values - aurora and redshift)\"}";
         }
         return new ResponseEntity<>(result.toJSON(), HttpStatus.OK);
-    }
-
-    DatabaseStoreResult queryAuroraStore(StoreAction action) {
-        return auroraStore.executeAction(action);
-    }
-
-    DatabaseStoreResult queryRedshiftStore(StoreAction action) {
-        return redshiftStore.executeAction(action);
     }
 }
